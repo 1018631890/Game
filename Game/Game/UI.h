0c651e6f596f01a459b3d6d870f922067a249a41
pureblack <1018631890@qq.com>
2018-12-12 21:06:44 +0800

    about UI

---
+++ Game/Game/UI.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "stdafx.h"
+#include<iostream>
+//others
+
+using namespace std;
+
+void menu()
+{
+	int k;
+	cout << "choose one game :" << endl;
+	cout << "1.Retro Snaker" << endl;
+	cin >> k;
+	if (k == 1)
+	{
+		//Game-Retro Snaker
+	}
+}
+
